ea3a6f50fa74448fe5719ef2c5cc1a5ff90fc439
JesnaJixon <122458773+JesnaJixon@users.noreply.github.com>
2023-01-11 21:29:23 +0530

    Add files via upload
    The file with the name "area" is a code for a python program that accepts the circle's radius from the user and computes the area.
    The file with the name "filename" is python program that prints the extension of the given file.

---
+++ area.py
@@ -0,0 +1,4 @@
+print("Hello")
+radius=float(input("Input the radius of the circle:\n"))
+print(" The area of the circle with radius  is :" + str(3.141592653589793 * radius**2))
+
---
+++ filenames.py
@@ -0,0 +1,5 @@
+filename=input('Input the Filename :')
+file_ext=filename.split(".")
+print("The  extension of the file is :  " + file_ext[-1])
+py="Python"
+
